a15d7b49ca9153e7b4e43f81a7c6445e53e08740
subinbab <subinbabu4127@gmail.com>
2022-03-29 17:01:51 +0530

    this is a sample project of team 4. an ecommerce website for reference of team 4members only contains a CRUD operation of product(basic template) and user registration api.

---
+++ UILayer1/Controllers/AdminController.cs
@@ -0,0 +1,79 @@
+﻿using DomainLayer;
+using DomainLayer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using UIlayer.Data.ApiServices;
+
+
+namespace UIlayer.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        Product data = null;
+        IConfiguration Configuration { get; }
+        ProductApi pr;
+        public AdminController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            pr = new ProductApi(Configuration);
+            data = new Product();
+        }
+        public ActionResult Index(int? i)
+        {
+            /*IEnumerable<Product> products = pr.GetProduct();*/
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product product = pr.GetProduct(id);
+            return View(product);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ViewProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                data.Id = 0;
+                data.Name = product.Name;
+                data.Model = product.Model;
+                data.Price = product.Price;
+                data.Description = product.Description;
+                bool result = pr.CreateProduct(data);
+            }
+            return RedirectToAction("");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Product product = pr.GetProduct(id);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                bool result = pr.EditProduct(product);
+            }
+            return RedirectToAction("");
+        }
+        public ActionResult Delete(int id)
+        {
+            bool result = pr.DeleteProduct(id);
+            if (result)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
